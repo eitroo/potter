4ef2581bd8432622394f2195f05a1ef6f305778b
eitroo <hoanng.ltvn@gmail.com>
2019-05-21 18:13:50 +0300

    d

--- potter2/detector/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from django.views.generic import RedirectView
-from . import views
-
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("detect", views.detect, name='detect'),
-]
