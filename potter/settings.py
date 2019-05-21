75beebb11da3e06f0a56ec4df27b9879e20678f6
eitroo <hoanng.ltvn@gmail.com>
2019-05-21 17:06:53 +0300

    d

--- potter/settings.py
+++ potter/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['potterrr.herokuapp.com']
+ALLOWED_HOSTS = ['']
 
 
 # Application definition
@@ -117,7 +117,4 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_TMP = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
